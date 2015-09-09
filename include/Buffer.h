022158126035f2652e91a0e068992b5d56921aeb
565687167@qq.com <565687167@qq.com>
2015-09-09 03:15:54 -0700

    fix error

---
+++ include/Buffer.h
@@ -0,0 +1,14 @@
+//
+// Created by root on 8/24/15.
+//
+
+#ifndef CPPNET_BUFFER_H
+#define CPPNET_BUFFER_H
+
+
+class Buffer {
+
+};
+
+
+#endif //CPPNET_BUFFER_H
---
+++ src/Buffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 8/24/15.
+//
+
+#include "Buffer.h"
