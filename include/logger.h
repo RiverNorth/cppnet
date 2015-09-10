aa06914880137ea6c30a9534577d017d16634afd
565687167@qq.com <565687167@qq.com>
2015-09-10 01:23:06 -0700

    add spdlog

---
+++ include/logger.h
@@ -0,0 +1,12 @@
+//
+// Created by root on 9/10/15.
+//
+
+#ifndef CPPNET_SPDLOG_H
+#define CPPNET_SPDLOG_H
+#include <iostream>
+#include "spdlog/spdlog.h"
+auto console = spdlog::stdout_logger_mt("console");
+
+
+#endif //CPPNET_SPDLOG_H
